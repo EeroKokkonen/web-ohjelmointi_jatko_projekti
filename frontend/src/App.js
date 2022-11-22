e8cdceca24080cd7c2e6fcf0c2860bc5f6d89596
Ilari Ijas <ilari.ijas@tuni.fi>
2022-11-22 18:43:51 +0200

    mainpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Navbar from "./components/Navbar.js";
+import {Routes, Route} from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import FoodmenuPage from "./pages/FoodmenuPage";
+import OrdersPage from "./pages/OrdersPage";
+import ProfilePage from "./pages/ProfilePage"
+
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='appi'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={ <MainPage /> } exact />
+        <Route path="/menu" element={ <FoodmenuPage /> } />
+        <Route path="/orders" element={ <OrdersPage /> } />
+        <Route path="/profile" element={ <ProfilePage /> } />
+
+      </Routes>
+
+    </div>
   );
 }
 
