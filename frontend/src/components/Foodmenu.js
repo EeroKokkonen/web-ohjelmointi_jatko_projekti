af75fab55340732fb10557989f365e7cc278f515
Ilari Ijas <ilari.ijas@tuni.fi>
2022-11-22 20:24:19 +0200

    2

--- frontend/src/components/Foodmenu.js
+++ frontend/src/components/Foodmenu.js
@@ -1,1 +1,20 @@
 // Ehkä joku alikomponentti vielä??
+
+const Foodmenu = () => {
+    const foodItems = [
+        {name:"Hampurilainen", price:"8,20e"},
+        {name:"Juustohampurilainen", price:"9,00e"}
+    ]
+    
+    const renderListOfFoods = (foods) => {
+        return foods.map(name => <h2>{foods.name}</h2>)
+    }
+
+    return(
+        <div>
+            {renderListOfFoods(foodItems)}
+        </div>
+    );
+};
+
+export default Foodmenu;
--- frontend/src/pages/FoodmenuPage.js
+++ frontend/src/pages/FoodmenuPage.js
@@ -1,8 +1,10 @@
+import Foodmenu from "../components/Foodmenu";
 
 const FoodmenuPage = () => {
   return(
     <div>
         <h2>MENU</h2>
+        <Foodmenu />
     </div>
   );
 };
