a5263fa5f65128e96c5f4ff708f15ea00e8d42d9
Ilari Ijas <ilari.ijas@tuni.fi>
2022-11-28 18:18:39 +0200

    2

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,5 +1,6 @@
 import Input from "./Input.js";
 import { useRef } from "react";
+import axios from "axios";
 
 const Modal = (props) => {
 
@@ -8,18 +9,25 @@ const Modal = (props) => {
   const errorColor = "rgba(255, 0, 30, 0.3)";
   const normalColor = "rgba(0, 0, 0, 0.7)";
 
-  const logInHandler = () =>{
+  const logInHandler = async () =>{
+    //Syötteen tarkistus, jos on tyhjät kentät
     if(userName.current.value === ""){
       userName.current.style.backgroundColor = errorColor;
     }else{
       userName.current.style.backgroundColor = normalColor;
     }
-      
     if(password.current.value === ""){
       password.current.style.backgroundColor = errorColor;
     }else{
       password.current.style.backgroundColor = normalColor;
     }
+
+    const user = {
+      email: userName.current.value,
+      password: password.current.value,
+    };
+
+    const response = await axios.post("/login", user);
   };
 
     return (
@@ -29,7 +37,7 @@ const Modal = (props) => {
         <Input label={"Salasana"} type={"password"} ref={password}/>
         <a href="/register" id="registerLink">Rekisteröidy</a>
         <button className="btn" onClick={logInHandler}>Confirm</button>
-        <button className="btn btn--alt" onClick={logInHandler}>Cancel</button>
+        <button className="btn btn--alt" onClick={props.onCancel}>Cancel</button>
       </div>
     );
   };
