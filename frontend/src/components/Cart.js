da4282d4b850efec608f0c20fbcc6aec7a672c5c
Ilari Ijas <ilari.ijas@tuni.fi>
2022-11-29 21:11:43 +0200

    ostoskori visuaalit

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import useToken from "../hooks/UseToken";
 
-const Cart = () => {
+const Cart = (props) => {
     const [foodItems, setFoodItems] = useState([]);
     const {token, setToken} = useToken("");
+    const [priceSum, setPriceSum] = useState(0);
 
     //Suoritetaan sivunlatauksessa
-    const fetchMenu = async () => {
+    const fetchCart = async () => {
+        let sum = 0;
         //Haetaan backendistä data
         const response = await axios.get("api/products/getShoppingCart/" + token);
         const fetchedFoods = [];
@@ -21,12 +23,17 @@ const Cart = () => {
                 price: response.data[key].price,
             });
         }
+
+        for (const key in fetchedFoods) {
+            sum += fetchedFoods[key].price;
+          }
+        
+        setPriceSum(sum);
         setFoodItems(fetchedFoods);
-        console.log(fetchedFoods);
     }
 
     useEffect(() => {
-        fetchMenu();
+        fetchCart();
     }, [])
 
     //mapataan array muuttujaan
@@ -56,6 +63,10 @@ const Cart = () => {
     return(
         <div>
             <section>{listFoods}</section>
+            <div className="sumBack">
+                <p className="sum">Yhteensä: { priceSum } €</p>
+            </div>
+            
         </div>
     );
 };
