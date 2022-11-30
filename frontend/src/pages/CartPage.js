39322bce6dd73bb2b989f989125164e1a0ef7a45
Eero <eero.kokkonen@hotmail.com>
2022-11-30 20:40:18 +0200

    Tuotteiden tilaus ja pyynti

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,14 +1,28 @@
 import Cart from "../components/Cart";
 import "./css/CartPage.css";
+import axios from "axios";
+import useToken from "../hooks/UseToken";
 
 const CartPage = () =>{
+    const {token} = useToken("");
 
+    const orderCart = async () => {
+        let response;
+        try{
+            console.log(token)
+            response = await axios.get("api/products/orderShoppingCart?email=" + token);
+            console.log(response.data);
+            
+        } catch (err){
+            console.log(response.data);
+        }
+    };
 
     return(
         <div>
             <h2 className="PageHeader">Ostoskori</h2>
             <Cart />
-            <button className="checkInButton">Kassalle</button>
+            <button className="checkInButton" onClick={orderCart}>Kassalle</button>
         </div>
     );
 };
